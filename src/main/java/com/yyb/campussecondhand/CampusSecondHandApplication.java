14271b5f770a5c0756f0d5757b2f30f3189f8e55
yanyibo <yanyibo108@163.com>
2022-01-26 20:41:18 +0800

    frist commit

---
+++ src/main/java/com/yyb/campussecondhand/CampusSecondHandApplication.java
@@ -0,0 +1,13 @@
+package com.yyb.campussecondhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSecondHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusSecondHandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yyb/campussecondhand/CampusSecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyb.campussecondhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSecondHandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
